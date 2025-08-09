bad2983e2fd3706864ab8155e9c93981804216df
rajkumaradhikari <rajkumaradhikarime@gmail.com>
2025-08-09 23:52:46 +0545

    Starting project

---
+++ src/main/java/com/example/demo/JavaExploreApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExploreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaExploreApplication.class, args);
+		System.out.println("Test"); //testing  project
+	}
+}
